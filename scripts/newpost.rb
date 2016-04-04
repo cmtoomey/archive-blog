854814e4d5edb2c3e7d0e7f5712b6015406b25f7
Chris Toomey <christ@zillowgroup.com>
2016-04-04 15:26:26 -0700

    updated files

--- scripts/newpost.rb
+++ scripts/newpost.rb
@@ -1,11 +1,12 @@
 require 'date'
 
-POSTS_DIR = '_posts/'
+POSTS_DIR = '_posts/'.freeze
 
 post = ARGV[0]
 
-File.open(POSTS_DIR + Date.today.strftime("%Y-%m-%d-") + post + '.md', 'w') {|f| f.write(
-  "---\nlayout: post\nsection-type: post\ntitle: Title\ncategory: Category\ntags: [ 'tag1', 'tag2' ]\n---")
-}
+File.open(POSTS_DIR + Date.today.strftime('%Y-%m-%d-') + 'newpost' + '.md', 'w') do |f|
+    f.write(
+        "---\nlayout: post\nsection-type: post\ntitle: Title\ncategory: Category\ntags: [ 'tag1', 'tag2' ]\n---")
+end
 
 puts('[+] Created ' + post + ' post')
